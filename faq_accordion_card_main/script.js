0233b2219bba82efe59d2a13b9c503f048bcf2b8
david ben-adeji <benadejidavid@gmail.com>
2023-02-07 10:59:34 +0100

    script complete

--- faq_accordion_card_main/script.js
+++ faq_accordion_card_main/script.js
@@ -1,10 +1,23 @@
-// 2.8125
 let sections = Array.from(document.querySelectorAll('.faq__section'));
 
 sections.forEach(section => {
-    let question = section.querySelector('.faq__question')
+    let question = section.querySelector('.faq__question');
+    let isCollapsed = true;
+    
     question.addEventListener('click', () => {
         let answer = section.querySelector('.faq__answer');
-        answer.classList.toggle('faq__answer--visible')
+        
+
+        if (isCollapsed) {
+            question.style.color = 'var(--very-dark-desaturated-blue)';
+            isCollapsed = false
+        } else {
+            question.style.color = '';
+            isCollapsed = true
+        }
+
+        question.classList.toggle('faq__question--rotate');
+        question.classList.toggle('faq__question--bold');
+        answer.classList.toggle('faq__answer--visible');
     })
 })
