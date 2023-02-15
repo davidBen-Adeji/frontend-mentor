45b71e7f526dcdb9d735e5fd318c4afbd8d8ac56
david ben-adeji <benadejidavid@gmail.com>
2023-02-15 14:45:51 +0100

    mobile design and script complete

---
+++ base_apparel_coming_soon-master/script.js
@@ -0,0 +1,12 @@
+let form = document.querySelector('.hero__form');
+let email = document.querySelector('.email');
+let error = document.querySelector('.error');
+let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+form.addEventListener('submit', (event) => {
+    if (email.value.match(validRegex))
+        return true;
+
+    error.classList.add('error--visible');
+    event.preventDefault();
+})
