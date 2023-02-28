94a4aa32d63521179bb7ad442e59d23607e7cf65
david ben-adeji <benadejidavid@gmail.com>
2023-02-28 10:55:32 +0100

    ..

--- interactive_rating_component/script.js
+++ interactive_rating_component/script.js
@@ -8,42 +8,40 @@ let resultOutput = document.querySelector('.result__span');
 let selectedRating = null;
 
 // add new styles to selected rating and store the rating value
-ratings.forEach((rating, index, array) => {
+ratings.forEach(rating => {
     rating.addEventListener('click', () => {
         // reset rating back to default color if it had been selected before
         if (selectedRating !== null) {
-            selectedRating.style.backgroundColor = '';
-            selectedRating.style.color = '';
+            selectedRating.classList.remove('rating__button--active');
         } 
        
     //    add new color and background color
-       rating.style.backgroundColor = 'var(--light-grey)';
-       rating.style.color = 'var(--white)';
+        rating.classList.add('rating__button--active');
+
     //    store the rating value
-       selectedRating = rating;
+        selectedRating = rating;
     })
 })
 
 submitButton.addEventListener('click', () => {
     // tell user to select a rating value if none has been selected
     if (selectedRating === null) {
-        noRating.style.display = 'block'
+        // noRating.style.display = 'block'
+        noRating.classList.remove('d-none');
         return;
     }
 
     // display rating value selected
-    ratingState.style.display = 'none';
-    thankYouState.style.display = 'block';
+    ratingState.classList.add('d-none');
+    thankYouState.classList.remove('d-none');
     resultOutput.innerText = selectedRating.dataset.value
 })
 
 // return to select another rating
 returnButton.addEventListener('click', () => {
-    thankYouState.style.display = 'none';
-    ratingState.style.display = 'block';
-    noRating.style.display = 'none';
-    selectedRating.style.backgroundColor = '';
-    selectedRating.style.color = '';
+    thankYouState.classList.add('d-none');
+    ratingState.classList.remove('d-none');
+    noRating.classList.add('d-none');
+    selectedRating.classList.remove('rating__button--active');
     selectedRating = null;
 })
-
