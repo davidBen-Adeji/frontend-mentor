56c75fcf0366bea1eb51dac588b75f40558a3a74
david ben-adeji <benadejidavid@gmail.com>
2023-03-10 15:21:18 +0100

    script complete

--- ping_coming_soon_page_master/script.js
+++ ping_coming_soon_page_master/script.js
@@ -1,1 +1,22 @@
 // The `input` field is empty. The message for this error should say *"Whoops! It looks like you forgot to add your email"*
+
+let submitForm = document.querySelector('[data-form]');
+let email = document.querySelector('[data-email]');
+let error = document.querySelector('[data-error]');
+let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+submitForm.addEventListener('submit', (event) => {
+    if (email.value == '') {
+        error.classList.remove('d-none')
+        error.textContent = 'Whoops! It looks like you forgot to add your email';
+        email.classList.add('intro__form__input--error');
+        email.value = '';
+        event.preventDefault();
+    }
+    else if (!(email.value.match(validRegex))) {
+        error.classList.remove('d-none')
+        error.textContent = 'Please provide a valid email';
+        email.classList.add('intro__form__input--error');
+        event.preventDefault();
+    }
+})
