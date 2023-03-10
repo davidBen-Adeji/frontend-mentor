afd033ffbe8c8cd822325faf1badc3bbff8a056a
david ben-adeji <benadejidavid@gmail.com>
2023-03-10 15:41:51 +0100

    updated README

--- ping_coming_soon_page_master/script.js
+++ ping_coming_soon_page_master/script.js
@@ -8,14 +8,14 @@ let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9
 submitForm.addEventListener('submit', (event) => {
     if (email.value == '') {
         error.classList.remove('d-none')
-        error.textContent = 'Whoops! It looks like you forgot to add your email';
+        error.innerText = 'Whoops! It looks like you forgot to add your email';
         email.classList.add('intro__form__input--error');
         email.value = '';
         event.preventDefault();
     }
     else if (!(email.value.match(validRegex))) {
         error.classList.remove('d-none')
-        error.textContent = 'Please provide a valid email';
+        error.innerText = 'Please provide a valid email';
         email.classList.add('intro__form__input--error');
         event.preventDefault();
     }
