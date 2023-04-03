690165529cb67285a2e8dd0bfb02786da9ac07f0
david ben-adeji <benadejidavid@gmail.com>
2023-04-03 20:00:51 +0100

    updated README

--- notifications-page-main/script.js
+++ notifications-page-main/script.js
@@ -2,10 +2,14 @@ const btn = document.querySelector('[data-btn]');
 const item = document.querySelectorAll('[data-item]');
 const notificationNum = document.querySelector('[data-num]');
 
-btn.addEventListener('click', () => {
-    item.forEach(item => {
-        item.classList.add('read');
+if (btn && item && notificationNum) {
+    btn.addEventListener('click', () => {
+        item.forEach(item => {
+            item.classList.add('read');
+        })
+    
+        notificationNum.innerText = '0';
     })
-
-    notificationNum.innerText = '0';
-})
+}
+else
+    console.error('Error: could not find necessary HTML elemnts');
