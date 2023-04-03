3aa954d3b07fae5205c3c5d29cd1c108caeeaaf1
david ben-adeji <benadejidavid@gmail.com>
2023-04-03 18:55:38 +0100

    design and js code complete

--- notifications-page-main/script.js
+++ notifications-page-main/script.js
@@ -1,1 +1,11 @@
-"use strict";
+const btn = document.querySelector('[data-btn]');
+const item = document.querySelectorAll('[data-item]');
+const notificationNum = document.querySelector('[data-num]');
+
+btn.addEventListener('click', () => {
+    item.forEach(item => {
+        item.classList.add('read');
+    })
+
+    notificationNum.innerText = '0';
+})
