c05101b9c6e647f67166119c5a4e00e4b3dad052
david ben-adeji <benadejidavid@gmail.com>
2023-05-30 17:05:10 +0100

    script

---
+++ interactive-card-details-form-main/script.js
@@ -0,0 +1,11 @@
+'use strict';
+const formParent = document.querySelector('.form-state');
+const form = document.querySelector('.form');
+const cvcOutput = document.querySelector('[data-cvc-output]');
+const numOutput = document.querySelector('[data-num-output]');
+const nameOutput = document.querySelector('[data-name-output]');
+const expOutput = document.querySelector('[data-exp-output]');
+
+form.addEventListener('click', () => {
+
+})
