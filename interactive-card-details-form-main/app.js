6e3569237d3aa5a58ec7f997eda16ce240ce5b2c
david ben-adeji <benadejidavid@gmail.com>
2023-07-12 15:03:03 +0100

    commit

--- interactive-card-details-form-main/app.js
+++ interactive-card-details-form-main/app.js
@@ -1,76 +1,154 @@
 // 'use strict';
-const formState = document.querySelector('[data-form-state]');
-const form = document.querySelector('[data-form]');
 const card = {
     num: document.querySelector('[data-num-output]'),
     name: document.querySelector('[data-name-output]'),
-    expDate: document.querySelector('[data-exp-output]'),
+    monthExpDate: document.querySelector('[data-month-exp]'),
+    yearExpDate: document.querySelector('[data-year-exp]'),
     cvc: document.querySelector('[data-cvc-output]'),
 }
-const formDeets = {
-    inputDivs: document.querySelectorAll('[data-input-div]'),
-    inputFields: document.querySelectorAll('[data-input-field]'),
-    errorMessages: document.querySelectorAll('[data-error-message]'),
-    nameField: document.querySelector('[data-name-field]'),
-    nameInput: document.querySelector('[data-input-name]'),
-    telephoneNum: document.querySelector('[data-input-cNum]'),
-    telephoneNumError: document.querySelector('[data-cNum-error]'),
-    numInputs: document.querySelectorAll('[data-input-num]'),
-    numErrors: document.querySelectorAll('[data-num-error]'),
-    monthInput: document.querySelector('[data-input-month]'),
-    yearInput: document.querySelector('[data-input-year]'),
-    monthYearError: document.querySelector('[data-exp-error]'),
-    cvcInput: document.querySelector('[data-input-cvc]'),
-    cvcError: document.querySelector('[data-cvc-error]'),
+
+const formDetails = {
+    nameDiv: document.querySelector('[data-name-div]'),
+    cardNumberDiv: document.querySelector('[data-cNum-div]'),
+    monthDiv: document.querySelector('[data-month-div]'),
+    yearDiv: document.querySelector('[data-year-div]'),
+    monthYearDiv: document.querySelector('[data-month-year-div]'),
+    cvcDiv: document.querySelector('[data-cvc-div]'),
 }
+
+const formState = document.querySelector('[data-form-state]');
+const form = document.querySelector('[data-form]');
+
 let isCardDetailsComplete;
 
 document.addEventListener('DOMContentLoaded', () => {
-    form.removeAttribute('novalidate');
+    form.setAttribute('novalidate', '');
 })
 
 const formValidation = e => {
     e.preventDefault();
 
-    // checkIfBlank(formDeets.inputDivs);
-    nameValidation(formDeets.nameField);
+    nameValidation(formDetails.nameDiv);
+    monthAndYearValidation(formDetails.monthYearDiv);
 }
 
 form.addEventListener('submit', formValidation)
 
-// const checkIfBlank = (inputDivs) => {
-//     for (const inputDiv of inputDivs) {
-//         const inputs = inputDiv.querySelectorAll('[data-input-field]');
-//         const errorMessage = inputDiv.querySelector('[data-error-message]');
-    
-//         for (const input of inputs) {
-//             if (!input.value.trim()) {
-//                 errorMessage.textContent = "Can't be blank";
-//                 input.classList.add('error');
-//             }
-//             else {
-//                 errorMessage.textContent = '';
-//                 input.classList.remove('error');
-//             }
-//         }
-//     }
-// }
+const nameValidation = (nameDiv) => {
+    const input = nameDiv.querySelector('[data-input-name]');
+    const errorMessage = nameDiv.querySelector('[data-name-error]');
 
-const checkIfBlank = (input, errorMessage) => {
+    if (isInputFieldBlank(input, errorMessage)) {
+        isCardDetailsComplete = false;
+        card.name.textContent = 'jane appleseed';
+    }
+    else {
+        isCardDetailsComplete = true;
+        card.name.textContent = input.value.trim();
+    } 
+}
+
+const monthAndYearValidation = (monthYearDiv) => {
+    const monthInput = monthYearDiv.querySelector('[data-input-month]');
+    const yearInput = monthYearDiv.querySelector('[data-input-year]');
+    const errorMessage = monthYearDiv.querySelector('[data-month-year-error]');
+    let isMonthOrYearBlank;
+    let isMontOrYearValidNum;
+    let isMonthOrYearInRange;
+
+    const blankField = (output) => {
+        isCardDetailsComplete = false;
+        errorMessage.textContent = "Can't be blank";
+        output.textContent = '00';
+    }
+
+    const invalidInput = (output) => {
+        isCardDetailsComplete = false;
+        errorMessage.textContent = 'Invalid Input';
+        output.textContent = '00';
+    }
+
+    const invalidRange = (output) => {
+        isCardDetailsComplete = false;
+        errorMessage.textContent = 'Invalid Range';
+        output.textContent = '00';
+    }
+
+    // returns true if number is between 1 and 12
+    const isMonthInRange = (input) => {
+        const num = input.value * 1;
+
+        if (num >= 1 && num <= 12) return true;
+
+        return false;
+    }
+
+    // returns true if number is between 0 and 99
+    const isYearInRange = (input) => {
+        const num = input.value * 1;
+
+        if (num >= 0 && num <= 99) return true;
+
+        return false;
+    }
+
+    if (isInputFieldBlank(monthInput, errorMessage)) {
+        isMonthOrYearBlank = true;
+        blankField(card.monthExpDate);
+    }
+    if (isInputFieldBlank(yearInput, errorMessage)) {
+        isMonthOrYearBlank = true;
+        blankField(card.yearExpDate);
+    }
+    if (isMonthOrYearBlank) return;
+
+
+
+    if (!isMonthInRange(monthInput)) {
+        console.log('not in range')
+        isMonthOrYearInRange = false;
+        invalidRange(card.monthExpDate);
+    }
+    if (!isYearInRange(yearInput)) {
+        isMonthOrYearInRange = false;
+        invalidRange(card.yearExpDate);
+    }
+    if (!isMonthOrYearInRange) return; 
+
+
+    
+    if (!isValidNum(monthInput)) {
+        isMontOrYearValidNum = false;
+        invalidInput(card.monthExpDate);
+    }
+    if (!isValidNum(yearInput)) {
+        isMontOrYearValidNum = false;
+        invalidInput(card.yearExpDate);
+    }
+    if (!isMontOrYearValidNum) return;   
+}
+
+// returns true if input field is blank
+const isInputFieldBlank = (input, errorMessage) => {
     if (!input.value.trim()) {
         errorMessage.textContent = "Can't be blank";
         input.classList.add('error');
+        return true;
     }
-    else {
+
         errorMessage.textContent = '';
         input.classList.remove('error');
-    }
+        return false;
 }
 
-const nameValidation = (nameField) => {
-    const input = nameField.querySelector('input');
-    const errorMessage = nameField.querySelector('[data-name-error]');
+// returns true if number is valid
+const isValidNum = (input) => {
+    const num = input.value * 1;
+    if (isNaN(num) || num % 1 !== 0) {
+        input.classList.add('error');
+        return false;
+    }
 
-    // should return true or false
-    checkIfBlank(input, errorMessage);
+    input.classList.remove('error');
+    return true;
 }
